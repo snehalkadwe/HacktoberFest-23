5465f1d6d3a2c30a1da3beae14c860d760d419b9
Abhishek Kashyap <akkashyap479@gmail.com>
2023-10-02 01:27:01 +0530

    superHeroPage

--- Contributors/ContributorsList.js
+++ Contributors/ContributorsList.js
@@ -3,5 +3,10 @@ contributors = [
       id: 1,
       fullname: "Ravi Kumar",
       username: "https://github.com/theravi04",
-    }
+    },
+    {
+      id: 2,
+      fullname: "Abhishek Kashyap",
+      username: "https://github.com/theabhishek26",
+    },
 ];
